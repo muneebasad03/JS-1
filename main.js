606e2756f8c73b6219f3e772ecd5d9e770189b9c
Muneeb Asad <107203420+muneebasad03@users.noreply.github.com>
2023-08-10 00:34:31 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-let WinnerPrize;
-WinnerPrize = 19;
 
-let GuessNumber = 
-+prompt("Guess number");
-
-if(GuessNumber === WinnerPrize){
-console.log("you won") }
-else{
-    console.log("wapisi khelo")
-}
