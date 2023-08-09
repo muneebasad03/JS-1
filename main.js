b83fbf04961f4eae58029926d48a9937a8afe1ad
muneebasad03 <muneebasad27@gmail.com>
2023-08-10 00:33:00 +0500

    Stock Vector Landing Page

---
+++ main.js
@@ -0,0 +1,11 @@
+let WinnerPrize;
+WinnerPrize = 19;
+
+let GuessNumber = 
++prompt("Guess number");
+
+if(GuessNumber === WinnerPrize){
+console.log("you won") }
+else{
+    console.log("wapisi khelo")
+}
